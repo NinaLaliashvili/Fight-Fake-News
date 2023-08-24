0a5e32b43906b635808be3e8721fef75e92113f8
efabs7 <118106689+efabs7@users.noreply.github.com>
2023-08-24 18:45:12 -0400

    re adding changes; former commit didn't add ef changes lol

--- fact-fiction/src/Components/LogIn/LogInComponent.jsx
+++ fact-fiction/src/Components/LogIn/LogInComponent.jsx
@@ -2,44 +2,81 @@ import { useContext, useState } from "react";
 import { authContext } from "../../Context/AuthContext";
 import "./LogInComponent.css";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import axios from "axios";
 
 const LogInComponent = () => {
   const navigate = useNavigate();
-  const { logUserIn } = useContext(authContext);
+  const { logUserIn, setUserInfo } = useContext(authContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const notifyError = (message) => {
+    toast.error(`${message}, sorry!`, {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
 
   const handleLogIn = () => {
-    //post email and password
-    //localStorage("LogInUser") (?)
-    //logUserIn(resp.data)
-    //catch
+    if (email && password) {
+      axios
+        .post(process.env.REACT_APP_API_PORT + "login", {
+          email,
+          password,
+        })
+        .then((resp) => {
+          console.log("made it to response");
+          const userObject = resp.data.user;
+          const { token, user } = userObject;
+          setUserInfo(user);
+          //getting user and token from server
+
+          logUserIn(token);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+          setErrorMessage(err);
+          notifyError(errorMessage);
+        });
+    } else {
+      setError(true);
+      notifyError(
+        "must enter values for email and password; c'mon, you know the drill!"
+      );
+    }
   };
   return (
     <div>
+      <ToastContainer theme="light" />
       <button onClick={() => navigate("/")}>Back to Home</button>
 
       <div className="loginContainer">
         <div className="login">
-          <h1>Login</h1>
+          <h1 className="login">Login</h1>
 
           <input
             type="email"
             value={email}
             onChange={setEmail}
             placeholder="Enter your email"
+            className="input"
           />
           <input
             type="password"
             value={password}
             onChange={setPassword}
             placeholder="Enter your password"
+            className="input"
           />
 
           <button onClick={handleLogIn}>Login</button>
-          <Link to="/register">Not a user? register</Link>
-          <div>{error && <div error={error} isError={true}></div>}</div>
+          <Link to="/register">Not a user? Register Here!</Link>
+
+          {/* <div>{error && <div error={error} isError={true}></div>}</div> */}
         </div>
       </div>
     </div>
--- fact-fiction/src/Components/Register/RegisterComponent.jsx
+++ fact-fiction/src/Components/Register/RegisterComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./RegisterComponent.css";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
 const RegisterComponent = () => {
   const [email, setEmail] = useState("");
@@ -11,17 +13,51 @@ const RegisterComponent = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
+  const notifyError = (message) => {
+    toast.error(`${message}, sorry!`, {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
+  const notifySuccess = (message) => {
+    toast.success(`${message}, sorry!`, {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
   const handleRegister = () => {
+    if (password === confirmPassword) {
+      axios
+        .post(process.env.REACT_APP_API_PORT + "signup", {
+          email: email,
+          name: name,
+          phone: phone,
+          password: password,
+        })
+        .then((resp) => {
+          console.log(resp);
+          notifySuccess("account created! You can now login");
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+          notifyError(err);
+        });
+    } else {
+      setError(true);
+      notifyError("passwords must match!");
+    }
     //post name, email, phone, password and confirmed password
     //show a message or navigate to the login page
     //catch error
   };
   return (
     <div>
+      <ToastContainer theme="light" />
       <button onClick={() => navigate("/")}>Back to Home</button>
 
-      <div className="loginContainer">
-        <div className="login">
+      <div className="registerContainer">
+        <div className="register">
           <h1>Register </h1>
 
           <input
@@ -29,6 +65,7 @@ const RegisterComponent = () => {
             value={name}
             onChange={setName}
             placeholder="Enter your name"
+            className="input"
           />
 
           <input
@@ -36,24 +73,28 @@ const RegisterComponent = () => {
             value={email}
             onChange={setEmail}
             placeholder="Enter your email"
+            className="input"
           />
           <input
             type="tel"
             placeholder="Phone Number"
             value={phone}
             onChange={setPhone}
+            className="input"
           />
           <input
             type="password"
             value={password}
             onChange={setPassword}
             placeholder="Enter your password"
+            className="input"
           />
           <input
             type="password"
             value={confirmPassword}
             onChange={setConfirmPassword}
             placeholder="Confirm Password"
+            className="input"
           />
 
           <button onClick={handleRegister}>Register</button>
