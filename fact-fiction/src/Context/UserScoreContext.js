e9f43901dc4ebdeb0fdbde40a2038bee6cbac861
efabs7 <118106689+efabs7@users.noreply.github.com>
2023-08-24 15:42:39 -0400

    added userScore context to be used for tallying userscore thru out quiz

---
+++ fact-fiction/src/Context/UserScoreContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const userScoreContext = createContext({});
+
+const Provider = userScoreContext.Provider;
+
+export const UserScoreProvider = ({ children }) => {
+  const [numOfCorrectAnswers, setNumOfCorrectAnswers] = useState(0);
+  const [numOfWrongAnswers, setNumOfWrongAnswers] = useState(0);
+  const [runningAverageScore, setRunningAverageScore] = useState(0);
+  /*runningAverageScore will equal numOfCorrect / numOfTotal as a percentage (yay math!) whenever we call it
+   */
+
+  const value = {
+    numOfCorrectAnswers,
+    setNumOfCorrectAnswers,
+    numOfWrongAnswers,
+    setNumOfWrongAnswers,
+    runningAverageScore,
+    setRunningAverageScore,
+  };
+
+  return <Provider value={value}>{children}</Provider>;
+};
